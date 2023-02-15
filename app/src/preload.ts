e3780b9afff61da9d322f3890f1448bc91a83652
Caleb Barger <bargercj20@hotmail.com>
2023-02-14 19:47:35 -0800

    Write script api

--- app/src/preload.ts
+++ app/src/preload.ts
@@ -1,9 +1,12 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("ElectronAPI", {
-	GetPyIncludePath: () => ipcRenderer.invoke("get-py-include-path"),
+contextBridge.exposeInMainWorld("ProtexAPI", {
+	GetScriptNames: () => ipcRenderer.invoke("get-script-names"),
     WriteConfig: (ConfigStr: string) => ipcRenderer.invoke("write-config", ConfigStr),
+    WriteScript: (ScriptName: string, Src: string) => ipcRenderer.invoke(
+        "write-script", ScriptName, Src),
+    ReadScript: (ScriptName: string) => ipcRenderer.invoke(
+        "read-script", ScriptName),
     RunExtractor: (ConfigName: string, Text: string) => ipcRenderer.invoke(
         "run-extractor", ConfigName, Text)
-
 });
